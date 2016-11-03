d162e2875a7c40a25768faa82012b5a74b809064
noblelady <csn6973@g.rit.edu>
2016-11-02 20:41:26 -0400

    Initial Commit of the After Hours site

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.rit-logos').click(function(){
+		document.location.href = "http://rit.edu";
+	});
+	$('.gccis-footer').click(function(){
+		document.location.href="http://rit.edu/gccis/";
+	});
+	//$('.register-button').click(function(){
+		//document.location.href="https://www.eventbrite.com/e/women-in-computing-wic-after-hours-tickets-10781905971";
+	//});
+//});
